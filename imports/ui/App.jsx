0897288c0f35e57d26d907d7a6a16f640318a625
Ivonig Corfmat <ivonig@collock.com>
2021-09-09 12:13:02 +0200

    added images

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,5 +8,9 @@ export const App = () => (
     <div className="text-gray-200">Styling test</div>
     <Hello/>
     <Info/>
+    <h2>Image relative</h2>
+    <img src='/revolin_dancing_guy.png' />
+    <h2>Image utilisant meteor.absoluteUrl()</h2>
+    <img src={`${Meteor.absoluteUrl()}revolin_dancing_guy.png`} />
   </div>
 );
