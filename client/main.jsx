3f881fd0ebb87f00735afb6efd26075a78de7bb6
Ivonig Corfmat <ivonig@collock.com>
2021-09-09 13:14:20 +0200

    changed image link - no routing

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { AppRouter } from '/imports/ui/App';
+import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
-  render(<AppRouter/>, document.getElementById('react-target'));
+  render(<App/>, document.getElementById('react-target'));
 });
