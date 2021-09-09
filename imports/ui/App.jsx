ee9074c3bf84bdba4d5569956e437bcfb22114b6
Ivonig Corfmat <ivonig@collock.com>
2021-09-09 09:47:33 +0200

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <div className="text-gray-200">Styling test</div>
+    <Hello/>
+    <Info/>
+  </div>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    "./client/**/*.html",
+    "./client/**/*.jsx",
+    "./client/**/*.js",
+    "./imports/ui/**/*.js",
+    "./imports/ui/**/*.jsx",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
